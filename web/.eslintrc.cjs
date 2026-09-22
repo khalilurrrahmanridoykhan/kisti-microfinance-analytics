afba8c6e8a96b55b72a2b8406ed3bee9a5800368
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2026-09-22 09:49:38 +0600

    Scaffold the dashboard: Vite, React, TypeScript, ESLint, Vitest

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-hooks", "react-refresh", "@typescript-eslint"],
+  ignorePatterns: ["dist", "*.cjs"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
